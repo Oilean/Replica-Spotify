5e40488b1343732a7ba5ba3915452381587379bc
William Santos <william.santos935@gmail.com>
2025-02-16 19:46:55 -0300

    ajustes para o deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,8 @@
 // Fetch ou Axios
 import axios from "axios";
 
-const URL = "http://localhost:3001";
+const { NODE_ENV } = process.env;
+const URL = "http://localhost:3001/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
@@ -9,4 +10,4 @@ const responseSongs = await axios.get(`${URL}/songs`);
 export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
 
-// console.log(responseArtists);
+// console.log(responseArtists.data);
